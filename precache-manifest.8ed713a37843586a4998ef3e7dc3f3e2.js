a4a2d81cad00c63a7359afa6712a84ebdaee5aeb
rodrigolazarte <romalaz94@gmail.com>
2020-02-21 15:34:51 -0300

    Updates

---
+++ precache-manifest.8ed713a37843586a4998ef3e7dc3f3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aace4a12268ebeb1d633eef5087a978",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "27420b2d63be3b709609",
+    "url": "/monster-app/static/css/main.62a3f3d0.chunk.css"
+  },
+  {
+    "revision": "1cbae4faf679499c2232",
+    "url": "/monster-app/static/js/2.7b422236.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-app/static/js/2.7b422236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27420b2d63be3b709609",
+    "url": "/monster-app/static/js/main.02bc103c.chunk.js"
+  },
+  {
+    "revision": "1d00325a76203b24d57a",
+    "url": "/monster-app/static/js/runtime-main.5929b07e.js"
+  }
+]);
